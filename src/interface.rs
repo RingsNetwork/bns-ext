2f201372fef95a23a3c1e68d136d3b7e81e68113
Ryan.K <ryankung@ieee.org>
2022-03-30 01:38:52 +0800

    handshake

--- src/interface.rs
+++ src/interface.rs
@@ -1,12 +1,15 @@
+// doc https://bugzilla.mozilla.org/show_bug.cgi?id=1659672
+
 use crate::discovery::SwarmConfig;
 use crate::web3::Web3Provider;
-
+use wasm_bindgen::JsValue;
 use anyhow::anyhow;
 use anyhow::Result;
 use bns_core::transports::Transport;
 use bns_core::dht::Chord;
 use bns_core::ecc::SecretKey;
 use bns_core::message::handler::MessageHandler;
+use bns_core::message::Encoded;
 use bns_core::swarm::Swarm;
 use bns_core::swarm::TransportManager;
 use bns_core::types::ice_transport::IceTrickleScheme;
@@ -24,15 +27,18 @@ pub struct MainView {
     pub key: SecretKey,
     pub msg_handler: Arc<MessageHandler>,
     pub pending_transport: Arc<Mutex<Option<Arc<Transport>>>>,
-    pub current_sdp: Arc<std::sync::Mutex<Option<String>>>,
-    sdp_input_ref: NodeRef,
+    pub current_offer: Arc<std::sync::Mutex<Option<String>>>,
+    pub current_answer: Arc<std::sync::Mutex<Option<String>>>,
+    offer_textarea: NodeRef,
+    answer_textarea: NodeRef,
     http_input_ref: NodeRef,
-    sdp_textarea_ref: NodeRef
 }
 
 pub enum Msg {
     ConnectPeerViaHTTP(String),
     ConnectPeerViaICE(String),
+    ResponseOffer(String),
+    AcceptAnswer(String),
     GenerateSdp,
     Update,
     None,
@@ -49,10 +55,11 @@ impl MainView {
             web3: Web3Provider::new(),
             key: cfg.key,
             pending_transport: Arc::new(Mutex::new(None)),
-            current_sdp: Arc::new(std::sync::Mutex::new(None)),
-            sdp_input_ref: NodeRef::default(),
+            current_offer: Arc::new(std::sync::Mutex::new(None)),
+            current_answer: Arc::new(std::sync::Mutex::new(None)),
+            offer_textarea: NodeRef::default(),
+            answer_textarea: NodeRef::default(),
             http_input_ref: NodeRef::default(),
-            sdp_textarea_ref: NodeRef::default()
         }
     }
 
@@ -128,41 +135,100 @@ impl Component for MainView {
                 let swarm = Arc::clone(&self.swarm);
                 let pending = Arc::clone(&self.pending_transport);
                 let sec_key = self.key.clone();
-                let current_sdp = Arc::clone(&self.current_sdp);
+                let current_offer = Arc::clone(&self.current_offer);
                 let link = ctx.link().clone();
                 spawn_local(async move {
                     match swarm.new_transport().await {
                         Ok(t) => {
                             match t.get_handshake_info(sec_key, web_sys::RtcSdpType::Offer).await {
                                 Ok(sdp) => {
-                                    log::debug!("setting sdp area");
+                                    log::debug!("setting sdp offer area");
                                     let mut p = pending.lock().await;
-                                    let mut s = current_sdp.lock().unwrap();
+                                    let mut s = current_offer.lock().unwrap();
                                     *p = Some(Arc::clone(&t));
                                     *s = Some(sdp.to_string());
                                     drop(p);
                                     drop(s);
-                                    log::debug!("done setting sdp area");
+                                    log::debug!("done setting sdp offer area");
                                     link.send_message(Msg::Update);
                                 },
                                 Err(e) => {
-                                    log::error!("cannot generate sdp offer {:?}", e)
+                                    log::error!("cannot generate sdp offer {:?}", e);
                                 }
                             }
                         },
                         Err(_) => {
-                            log::error!("failed to setting pending transport")
+                            log::error!("failed to setting pending transport");
                         }
                     }
                 });
                 log::debug!("should update");
                 false
             }
-            Msg::None => false,
+            Msg::ResponseOffer(s) => {
+                log::debug!("get offer {:?}", s.clone());
+                let swarm = Arc::clone(&self.swarm);
+                let offer = s.clone();
+                let pending = Arc::clone(&self.pending_transport);
+                let current_answer = Arc::clone(&self.current_answer);
+                let link = ctx.link().clone();
+                let sec_key = self.key.clone();
+
+                spawn_local(async move {
+                    match swarm.new_transport().await {
+                        Ok(t) => {
+                            match t.register_remote_info(Encoded(offer)).await {
+                                Ok(addr) => {
+                                    let sdp = t.get_handshake_info(sec_key, web_sys::RtcSdpType::Answer).await.unwrap();
+                                    swarm.register(&addr, t.clone()).await.unwrap();
+                                    log::debug!("setting sdp answer area");
+                                    let mut p = pending.lock().await;
+                                    let mut s = current_answer.lock().unwrap();
+                                    *p = Some(Arc::clone(&t));
+                                    *s = Some(sdp.to_string());
+                                    drop(p);
+                                    drop(s);
+                                    log::debug!("done setting sdp answer area");
+                                    link.send_message(Msg::Update);
+
+
+                                },
+                                Err(e) => {
+                                    log::error!("cannot generate sdp answer {:?}", e);
+
+                                }
+                            }
+                        },
+                        Err(e) => {
+                            log::error!("cannot generate new transport {:?}", e);
+                        }
+                    };
+                });
+                false
+            }
+            Msg::AcceptAnswer(s) => {
+                let pending = Arc::clone(&self.pending_transport);
+                // if pending.is_none() {
+                //     log::error!("cannot find pending transport, maybe you should create offer first");
+                // }
+                let swarm = Arc::clone(&self.swarm);
+                let answer = s.clone();
+                let link = ctx.link().clone();
+
+                spawn_local(async move {
+                    if let Some(t) = &*pending.lock().await {
+                        let addr = t.register_remote_info(Encoded(answer)).await.unwrap();
+                        swarm.register(&addr, t.clone()).await.unwrap();
+                        link.send_message(Msg::Update);
+                    }
+                });
+                false
+            }
             Msg::Update => {
                 log::debug!("force update!");
                 true
             }
+            _ => false
         }
     }
 
@@ -175,19 +241,7 @@ impl Component for MainView {
             <body>
                 <div id="viewport">
                 <p>
-                <input ref={self.sdp_input_ref.clone()} id="remote_sdp_field" type="text" />
-                <button onclick={
-                    let input = self.http_input_ref.clone();
-                    ctx.link().callback(move |_| {
-                        match input.cast::<HtmlInputElement>() {
-                            Some(input) => Msg::ConnectPeerViaICE(input.value()),
-                            None => Msg::None
-                        }
-                    })
-                }>{"Connect with SDP Swap"}</button>
-                </p>
-                <p>
-                <input ref={self.http_input_ref.clone()}id="remote_http_field" type="text" />
+                <input ref={self.http_input_ref.clone()} type="text" />
                 <button onclick={
                     let input = self.http_input_ref.clone();
                     ctx.link().callback(move |_| {
@@ -198,12 +252,51 @@ impl Component for MainView {
                     })
                 }>{"Connect To Entry Node"}</button>
                 </p>
+                <div>
+                <h2>{"Handshake with SDP Exchange"}</h2>
                 <p>
-                <pre ref={self.sdp_textarea_ref.clone()}>{
-                    (*self.current_sdp.lock().unwrap()).as_ref().unwrap_or(&"".to_string())
+                <h3>{"Create Offer"}</h3>
+                <button onclick={ctx.link().callback(move |_| Msg::GenerateSdp)}>{"Generate Handshake Offer"}</button>
+                <pre>{
+                    (*self.current_offer.lock().unwrap()).as_ref().unwrap_or(&"".to_string())
                 }</pre>
-                <button onclick={ctx.link().callback(move |_| Msg::GenerateSdp)}>{"Generate Handshake SDP"}</button>
+
                 </p>
+                <p>
+                <h3>{"Responser and Create Answer"}</h3>
+                <textarea ref={self.offer_textarea.clone()} type="text" ></textarea>
+                <button onclick={
+                    let input = self.offer_textarea.clone();
+                    log::info!("select textarea {:?}", &input);
+                    ctx.link().callback(move |_| {
+                        match input.cast::<web_sys::HtmlTextAreaElement>() {
+                            Some(textarea) => {
+                                Msg::ResponseOffer(textarea.value())
+                            },
+                            None => Msg::None
+                        }
+                    })
+                }>{"Response Offer"}</button>
+                <pre>{
+                    (*self.current_answer.lock().unwrap()).as_ref().unwrap_or(&"".to_string())
+                }</pre>
+                </p>
+
+                <p>
+                <h3>{"Accept Answer"}</h3>
+                <textarea ref={self.answer_textarea.clone()} type="text" ></textarea>
+                <button onclick={
+                    let input = self.answer_textarea.clone();
+                    ctx.link().callback(move |_| {
+                        match input.cast::<web_sys::HtmlTextAreaElement>() {
+                            Some(input) => Msg::AcceptAnswer(input.value()),
+                            None => Msg::None
+                        }
+                    })
+                }>{"Accept Answer"}</button>
+                </p>
+
+                </div>
                 </div>
             </body>
         }
